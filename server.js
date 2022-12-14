804087c85e4850203f012d08a7004bcaf95ba361
sarthak2823 <sarthakgoel1234@gmail.com>
2022-12-14 23:20:37 +0530

    cors added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
+var cors = require("cors");
+app.use(cors());
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
